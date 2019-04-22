5925db9c1d880470cb651b65967dc750a4a81c48
fhk <fuwu1210@gmail.com>
2019-04-22 15:49:08 +0800

    4.22-15:48

--- src/Day_01/MyStartPanel.java
+++ src/Day_01/MyStartPanel.java
@@ -8,27 +8,73 @@ import java.util.Random;
  * @author fhk
  * @date 2019/4/22-10:56
  */
-public class MyStartPanel extends JPanel {
-    public void paint(Graphics g){
+/*
+ * 线程和进程
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * */
+public class MyStartPanel extends JPanel implements Runnable {
+
+    int[] xx = new int[100];//全局变量
+    int[] yy = new int[100];//全局变量
+
+    /*
+     * 构造方法
+     * 只执行一次
+     * 在new mystattpanel调用
+     * 不写系统会自动创建
+     *
+     * */
+    public MyStartPanel() {
+        for (int i = 0; i < 100; i++) {
+            xx[i] = (int) (Math.random() * 800);
+            yy[i] = (int) (Math.random() * 600);
+        }//数组初始化
+
+    }
+
+    public void paint(Graphics g) {
         super.paint(g);//g画笔,继承
 
         this.setBackground(Color.BLACK);//当前该对象背景颜色
 
-        Font ft =new Font("微软雅黑",Font.BOLD,28);
+
+        //5：设置月亮
+        g.setColor(Color.GRAY);
+        g.fillOval(66, 66, 66, 100);//x,y,w,h,(x,y)左上角坐标，w宽，h
+
+        g.setColor(Color.black);
+        g.fillOval(44, 44, 66, 100);//x,y,w,h,(x,y)左上角坐标，w宽，h高
+
+
+        Font ft = new Font("微软雅黑", Font.BOLD, 28);
 
         g.setFont(ft);
 
 
         for (int i = 0; i < 100; i++) {
-            int x= (int)(Math.random()*800);
-            int y= (int)(Math.random()*600);
+            int x = (int) (Math.random() * 800);
+            int y = (int) (Math.random() * 600);
             //五颜六色
-            int R=(int)(Math.random()*256);
-            int G=(int)(Math.random()*256);
-            int B=(int)(Math.random()*256);
-            Color randColor=new Color(R,G,B);
+            int R = (int) (Math.random() * 256);
+            int G = (int) (Math.random() * 256);
+            int B = (int) (Math.random() * 256);
+            Color randColor = new Color(R, G, B);
             g.setColor(randColor);
-            g.drawString(".",x,y);
+            g.drawString("你滴寒王", xx[i], yy[i]);
             //g.drawString("*",1000-i,i);
 
         }
@@ -38,5 +84,29 @@ public class MyStartPanel extends JPanel {
         /*for (int i = 0; i <500 ; i++) {
             g.drawString("龙鸣",i,i);
         }*/
+    }
+
+    public void run() {
+        while (true) {
+            //1业务执行
+            /*流星雨*/
+            for (int i = 0; i < 100; i++) {
+                xx[i]++;
+                //yy[i]++;
+                if (xx[i] > 800)
+                    xx[i] = 0;
+                if (yy[i] > 600)
+                    yy[i] = 0;
+            }
+            //2睡眠方法
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            //3调用重绘方法
+            repaint();//擦掉，重绘，一直循环
+        }
     }
 }
--- src/Day_01/Mystart.java
+++ src/Day_01/Mystart.java
@@ -10,10 +10,16 @@ import java.util.ArrayList;
 public class Mystart {
     public static void main(String[] args) {
         JFrame frame = new JFrame();//新建对象
-        MyStartPanel panel=new MyStartPanel();//新建panel
+        /*画布类*/
+        MyStartPanel panel = new MyStartPanel();//新建panel
         frame.add(panel);//在画板上添加画纸
 
+        /*线程关联*/
+        Thread thread = new Thread(panel);//线程启动
+        thread.start();
 
+
+        //相关属性设置
         frame.setSize(800, 600);//尺寸
         frame.setVisible(true);//可见性
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//关闭模式
